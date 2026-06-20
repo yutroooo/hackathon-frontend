866af3326ffd2b6c63cef8f339e72e264199bf58
yutaro <kun195342@gmail.com>
2026-06-21 01:04:22 +0900

    ab

--- hackathon-frontend/src/App.tsx
+++ hackathon-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { api } from "./api";
+import "./index.css";
 
 export default function App() {
   // ========================================================
